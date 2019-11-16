3505268bce34457dfeefa233ac7470031034f37d
John Alexis Munera <alexismunera98@gmail.com>
2019-11-16 16:23:11 -0500

    Added list command

--- herokubot.py
+++ herokubot.py
@@ -4,14 +4,36 @@ from requests_html import HTMLSession
 from requests import post
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+dict_products = {}
+
 
 def start(bot, update):
     update.effective_message.reply_text("Bienvenido al robot de la tienda Brahma!\n"
                                         "Soy util para que sepas los informacion sobre la web.\n\n"
                                         "Mis comandos disponibles son:\n"
-                                        "/lista - Lista los precios de la web"
-                                        "/foto id - Muestra la foto del id seleccionado"
-                                        "/promedio - Muestra el precio promedio de los productos")
+                                        "/lista - Lista los precios de la web\n"
+                                        "/foto id - Muestra la foto del id seleccionado\n"
+                                        "/promedio - Muestra el precio promedio de los productos\n")
+
+
+def list_products(update, context):
+    session = HTMLSession()
+    if len(dict_products) != 30:
+        page = session.get('https://www.brahma.co/es/tienda/hombre')
+        if page.status_code == 200:
+            all_items = page.html.find('a.product')
+            for id, item in enumerate(all_items):
+                dict_products[id] = {
+                    'nombre': item.find('.name', first=True).text,
+                    'precio': item.find('.price', first=True).text,
+                    'img_url': 'https://www.brahma.co{imagen}'.format(
+                        imagen=item.find('img', first=True).attrs.get('src'))
+                }
+    list_response = ''
+    for id, product in dict_products.items():
+        list_response += '{id}. Nombre: {nombre}\n' \
+                         'Precio: {precio}\n\n'.format(id=id, **product)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=list_response)
 
 
 def echo(bot, update):
@@ -59,6 +81,7 @@ if __name__ == "__main__":
     # Add handlers
     dp.add_handler(CommandHandler('start', start))
     dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_handler(CommandHandler('list', list_products))
     dp.add_error_handler(error)
 
     # Start the webhook
--- test.py
+++ test.py
@@ -3,13 +3,19 @@ from requests_html import HTMLSession
 session = HTMLSession()
 page = session.get('https://www.brahma.co/es/tienda/hombre')
 # page.html.render()
+dict_products = {}
 if page.status_code == 200:
-    all_items = page.html.find('.item-info')
-    for item in all_items:
-        print(item.find('.name', first=True).text)
-        print(item.find('.price', first=True).text)
+    all_items = page.html.find('a.product')
+    for id, item in enumerate(all_items):
+        dict_products[id] = {
+            'nombre': item.find('.name', first=True).text,
+            'precio': item.find('.price', first=True).text,
+            'img_url': 'https://www.brahma.co{imagen}'.format(imagen=item.find('img', first=True).attrs.get('src'))
+        }
 
-    print(all_items)
-    price_founded = page.html.find('.price', first=True).text
-    # discount = page.html.find('.price-discount-percent', first=True).text
-    print(price_founded)
+list_response = ''
+for id, product in dict_products.items():
+    list_response += '{id}. Nombre: {nombre}\n' \
+                     'Precio: {precio}\n\n'.format(id=id, **product)
+print(list_response)
+# html.ng-scope body#ProductsList div.content.lang-es main.products-controller.index div.products div.col a.product.ng-isolate-scope
